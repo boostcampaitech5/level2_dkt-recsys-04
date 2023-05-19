46780d00d04046281141b4d661b7134a6b5cf396
y2r1m <yerim22565974@gmail.com>
2023-05-19 18:23:43 +0900

    feat: add CV

--- code/catboost/catboost_util/args.py
+++ code/catboost/catboost_util/args.py
@@ -10,12 +10,8 @@ def train_parse_args() -> argparse.Namespace:
         "--use_cuda_if_available", default=True, type=bool, help="Use GPU"
     )
 
-    parser.add_argument(
-        "--data_dir", default="/opt/ml/input/data", type=str, help=""
-    )
-    parser.add_argument(
-        "--model_dir", default="./models/", type=str, help="model dir"
-    )
+    parser.add_argument("--data_dir", default="/opt/ml/input/data", type=str, help="")
+    parser.add_argument("--model_dir", default="./models/", type=str, help="model dir")
     parser.add_argument(
         "--output_dir", default="./outputs/", type=str, help="output dir"
     )
@@ -25,7 +21,19 @@ def train_parse_args() -> argparse.Namespace:
     )
     parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
     parser.add_argument("--depth", default=6, type=int, help="depth")
-
+    parser.add_argument("--use_kfold", default=True, type=bool, help="Use K-Fold CV")
+    parser.add_argument(
+        "--use_skfold", default=False, type=bool, help="Use Stratified K-Fold CV"
+    )
+    parser.add_argument(
+        "--use_tscv", default=False, type=bool, help="Use Time Series CV"
+    )
+    parser.add_argument(
+        "--use_btscv", default=False, type=bool, help="Use Blocking Time Series CV"
+    )
+    parser.add_argument(
+        "--n_splits", default=5, type=int, help="n_splits in Cross-Validation"
+    )
     args = parser.parse_args()
 
     return args
@@ -37,15 +45,11 @@ def inference_parse_args() -> argparse.Namespace:
 
     parser.add_argument("--seed", default=42, type=int, help="seed")
 
-    parser.add_argument(
-        "--data_dir", default="/opt/ml/input/data", type=str, help=""
-    )
+    parser.add_argument("--data_dir", default="/opt/ml/input/data", type=str, help="")
     parser.add_argument(
         "--output_dir", default="./outputs/", type=str, help="output dir"
     )
-    parser.add_argument(
-        "--model_dir", default="./models/", type=str, help="model dir"
-    )
+    parser.add_argument("--model_dir", default="./models/", type=str, help="model dir")
     parser.add_argument(
         "--model_name", default="catboost.cbm", type=str, help="output dir"
     )
--- code/catboost/catboost_util/datasets.py
+++ code/catboost/catboost_util/datasets.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from typing import Tuple
 import random
@@ -67,9 +68,7 @@ def custom_train_test_split(
     Returns:
         - Tuple[pd.DataFrame, pd.DataFrame]: Train , Valid Data Set
     """
-    users = list(
-        zip(df["userID"].value_counts().index, df["userID"].value_counts())
-    )
+    users = list(zip(df["userID"].value_counts().index, df["userID"].value_counts()))
     random.shuffle(users)
 
     max_train_data_len = ratio * len(df)
@@ -102,3 +101,72 @@ def custom_label_split(df: pd.DataFrame) -> Tuple[list, pd.DataFrame]:
     y = df["answerCode"]
     X = df.drop(["answerCode"], axis=1)
     return y, X[FEATS]
+
+
+# custom => 아직 사용 X
+def custom_kfold_split(df: pd.DataFrame, n_splits: int = 5) -> Tuple[list, list]:
+    """_summary_
+
+    Args:
+        -
+    Returns:
+        -
+    """
+    # 1. 레이블 분포에 맞게 레이블별로 분류
+    # 2. 유저별로 데이터셋 split
+    # 3. train, valid idx 반환
+
+    # shuffle=True
+    # random_state=42
+
+    # custom_train_test_split을 n_splits-1만큼 반복해서 얻어지는 n_split개의 dataset을 kfold 데이터셋으로 사용
+    # ratio = round(n_splits / len(df), 1)
+    # for i in range(n_splits-1):
+    #     train, tmp = custom_train_test_split(tmp, ratio=ratio)
+    # test = tmp[tmp["userID"] != tmp["userID"].shift(-1)]
+
+    users = list(zip(df["userID"].value_counts().index, df["userID"].value_counts()))
+    random.shuffle(users)
+
+    max_train_data_len = len(df) // n_splits
+    tmp = df.copy()
+    for n in range(n_splits - 1):  # n_splits개 만큼 나눠야 하므로 n_splits-1번 반복해서 나눔.
+        # train을 고르고, 나머지(tmp)는 또 다시 다음 반복에서 나눌 df가 된다. 마지막 반복에 남는 것이 test가 된다.
+        sum_of_train_data = 0
+        user_ids = []
+
+        for user_id, count in users:
+            sum_of_train_data += count
+            if max_train_data_len < sum_of_train_data:
+                break
+            user_ids.append(user_id)
+
+        train = tmp[tmp["userID"].isin(user_ids)]
+        test = df[df["userID"].isin(user_ids) == False]
+
+        # test데이터셋은 각 유저의 마지막 interaction만 추출
+        test = test[test["userID"] != test["userID"].shift(-1)]
+        tmp = tmp[tmp["userID"].isin(user_ids) == False]
+
+    test = tmp[tmp["userID"] != tmp["userID"].shift(-1)]
+    return (train.index, test.index)
+
+
+class BlockingTimeSeriesSplit:
+    def __init__(self, n_splits=None):
+        self.n_splits = n_splits
+
+    def get_n_splits(self, X, y, groups):
+        return self.n_splits
+
+    def split(self, X, y=None, groups=None):
+        n_samples = len(X)
+        k_fold_size = n_samples // self.n_splits
+        indices = np.arange(n_samples)
+
+        margin = 0
+        for i in range(self.n_splits):
+            start = i * k_fold_size
+            stop = start + k_fold_size
+            mid = int(0.8 * (stop - start)) + start
+            yield indices[start:mid], indices[mid + margin : stop]
