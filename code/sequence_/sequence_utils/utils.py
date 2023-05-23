f755a7f815fc026c78bea8a25c15774c09e7fe1d
임도현_T5170 <86715604+limstonestone@users.noreply.github.com>
2023-05-23 17:28:07 +0900

    fix : modify hptune-sequence
    * chore : update README.md
    
    * chore : modfiy README.md
    
    * refactor : modify tune.py
    
    * fix : modify tune.py
    
    * chore : modify tune.py

--- code/sequence_/sequence_utils/utils.py
+++ code/sequence_/sequence_utils/utils.py
@@ -2,7 +2,10 @@ import os
 import random
 
 import numpy as np
+import pandas as pd
+
 import torch
+import hyperopt
 
 
 class process:
@@ -56,3 +59,70 @@ logging_conf = {  # only used when 'user_wandb==False'
     },
     "root": {"level": "INFO", "handlers": ["console", "file_handler"]},
 }
+
+
+# 하이퍼파라미터튜닝 결과 저장 함수
+def trials_to_df(trials, space, best):
+    # 전체 결과
+    rows = []
+    keys = list(trials.trials[0]["misc"]["vals"].keys())
+
+    # 전체 실험결과 저장
+    for trial in trials:
+        row = {}
+
+        # tid
+        tid = trial["tid"]
+        row["experiment"] = str(tid)
+
+        # hyperparameter 값 저장
+        vals = trial["misc"]["vals"]
+        hparam = {key: value[0] for key, value in vals.items()}
+
+        # space가 1개 - 값을 바로 반환
+        # space가 다수 - dict에 값을 반환
+        hparam = hyperopt.space_eval(space, hparam)
+
+        if len(keys) == 1:
+            row[keys[0]] = hparam
+        else:
+            for key in keys:
+                row[key] = hparam[key]
+
+        # metric
+        row["metric"] = abs(trial["result"]["loss"])
+
+        # 소요 시간
+        row["time"] = (trial["refresh_time"] - trial["book_time"]).total_seconds()
+
+        rows.append(row)
+
+    experiment_df = pd.DataFrame(rows)
+
+    # best 실험
+    row = {}
+    best_hparam = hyperopt.space_eval(space, best)
+
+    if len(keys) == 1:
+        row[keys[0]] = best_hparam
+    else:
+        for key in keys:
+            row[key] = best_hparam[key]
+    row["experiment"] = "best"
+
+    best_df = pd.DataFrame([row])
+
+    # best 결과의 auc / time searching 하여 찾기
+    search_df = pd.merge(best_df, experiment_df, on=keys)
+
+    # column명 변경
+    search_df = search_df.drop(columns=["experiment_y"])
+    search_df = search_df.rename(columns={"experiment_x": "experiment"})
+
+    # 가장 좋은 metric 결과 중 가장 짧은 시간을 가진 결과를 가져옴
+    best_time = search_df.time.min()
+    search_df = search_df.query("time == @best_time")
+
+    df = pd.concat([experiment_df, search_df], axis=0)
+
+    return df
