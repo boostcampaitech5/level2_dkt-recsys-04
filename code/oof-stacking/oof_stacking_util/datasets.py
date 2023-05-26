1afd33fd9345525c26e690d80d417ba03bc44b15
sumin <ksum.jerry@gmail.com>
2023-05-26 12:03:02 +0900

    feat : update datasets.py
    change seed
    add elapsed_question feature
    update preprocessing tp change 'np.nan' tp 'unknwon for unknown class
    add feature_engineering
    
    '#34'

--- code/oof-stacking/oof_stacking_util/datasets.py
+++ code/oof-stacking/oof_stacking_util/datasets.py
@@ -12,19 +12,20 @@ from sklearn.preprocessing import LabelEncoder
 
 
 class Preprocess:
-    def __init__(self, args):
+    def __init__(self, args, is_train=True):
         self.args = args
         self.train_data = None
+        self.is_train = is_train
 
     def get_train_data(self):
         return self.train_data
 
-    def split_data(self, data, ratio=0.7, shuffle=True, seed=0):
+    def split_data(self, data, ratio=0.9, shuffle=False, seed=42):
         """
         split data into two parts with a given ratio.
         """
         if shuffle:
-            random.seed(seed)  # fix to default seed 0
+            random.seed(seed)  # fix to default seed 42
             random.shuffle(data)
 
         size = int(len(data) * ratio)
@@ -39,17 +40,30 @@ class Preprocess:
         np.save(le_path, encoder.classes_)  # 클래스에 대한 라벨 인코딩된 값
 
     def __preprocessing(self, df):  # 전처리
-        cate_cols = ["assessmentItemID", "testId", "KnowledgeTag"]  # 카테고리형 변수
+        cate_cols = [
+            "assessmentItemID",
+            "testId",
+            "KnowledgeTag",
+            "elapsed_question",
+        ]  # 카테고리형 변수
         for col in cate_cols:
-            # For UNKNOWN class
-            a = df[col].unique().tolist() + [
-                np.nan
-            ]  # 결측치 추가 -> 결측치도 라벨 인코딩 과정에서 unique 정수 인덱스로 매핑되게 하기 위해서
-
             le = LabelEncoder()  # 레이블 인코딩(카테고리형 변수 -> 숫자값)
-            le.fit(a)
+            if self.is_train:
+                # For UNKNOWN class
+                a = df[col].unique().tolist() + [
+                    "unknown"
+                ]  # 결측치 추가 -> 결측치도 라벨 인코딩 과정에서 unique 정수 인덱스로 매핑되게 하기 위해서
+                le.fit(a)
+                self.__save_labels(le, col)
+            else:
+                label_path = os.path.join(f"/opt/ml/input/data", col + "_classes.npy")
+                le.classes_ = np.load(label_path)
+                df[col] = df[col].apply(
+                    lambda x: x if str(x) in le.classes_ else "unknown"
+                )
+
+            df[col] = df[col].astype(str)
             df[col] = le.transform(df[col])
-            self.__save_labels(le, col)
 
         def convert_time(s):
             timestamp = time.mktime(
@@ -61,19 +75,46 @@ class Preprocess:
 
         return df
 
+    def __feature_engineering(self, df: pd.DataFrame) -> pd.DataFrame:
+        # 문자 -> 날짜 형식으로 변환
+        df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+        # 문항별 걸린 시간 (초)
+        df["elapsed_question"] = df.groupby(["userID", "testId"])["Timestamp"].diff()
+        df["elapsed_question"] = df["elapsed_question"].map(
+            lambda x: x.seconds
+        )  # 초 단위로 저장
+        df["elapsed_question"].fillna(
+            df["elapsed_question"].mode()[0], inplace=True
+        )  # 최빈값으로 결측치 대체
+        df["elapsed_question"].map(
+            lambda x: 259200 if x > 259200 else x
+        )  # 최대 3일(=259200초)로 clip
+        # 시험지별 걸린 시간 (초)
+        # df["elapsed_test"] = df.groupby(["userID", "testId"])["elapsed_question"].transform("sum")  # 문항별 걸린 시간을 합침
+        df["Timestamp"] = df["Timestamp"].map(lambda x: str(x))  # 다시 문자열로 변환
+        return df
+
     def load_data_from_file(self, file_name):
         csv_file_path = os.path.join(self.args.data_dir, file_name)
         print(self.args.data_dir)
         df = pd.read_csv(csv_file_path)
+        df = self.__feature_engineering(df)
         df = self.__preprocessing(df)
 
         # 추후 feature를 embedding할 시에 embedding_layer의 input 크기를 결정할때 사용
-        self.args.n_questions = df["assessmentItemID"].nunique()  # 고유 문항 수: 총 9454개
-        self.args.n_test = df["testId"].nunique()  # 고유 시험지의 수: 총 1537개
-        self.args.n_tag = df["KnowledgeTag"].nunique()  # 고유 태그의 수: 총 912개
+        # self.args.n_questions = df["assessmentItemID"].nunique()  # 고유 문항 수: 총 9454개
+        # self.args.n_test = df["testId"].nunique()  # 고유 시험지의 수: 총 1537개
+        # self.args.n_tag = df["KnowledgeTag"].nunique()  # 고유 태그의 수: 총 912개
 
         df = df.sort_values(by=["userID", "Timestamp"], axis=0)  # user별 시간순으로 정렬
-        columns = ["userID", "assessmentItemID", "testId", "answerCode", "KnowledgeTag"]
+        columns = [
+            "userID",
+            "assessmentItemID",
+            "testId",
+            "answerCode",
+            "KnowledgeTag",
+            "elapsed_question",
+        ]
         group = (
             df[columns]
             .groupby("userID")
@@ -83,6 +124,7 @@ class Preprocess:
                     r["assessmentItemID"].values,
                     r["KnowledgeTag"].values,
                     r["answerCode"].values,
+                    r["elapsed_question"].values,
                 )
             )
         )
@@ -104,9 +146,15 @@ class DKTDataset(torch.utils.data.Dataset):
         # 각 data의 sequence length
         seq_len = len(row[0])
 
-        test, question, tag, correct = row[0], row[1], row[2], row[3]
+        test, question, tag, correct, elapsed_question = (
+            row[0],
+            row[1],
+            row[2],
+            row[3],
+            row[4],
+        )
 
-        cate_cols = [test, question, tag, correct]
+        cate_cols = [test, question, tag, correct, elapsed_question]
 
         # max seq len을 고려하여서 이보다 길면 자르고 아닐 경우 그대로 냅둔다
         if seq_len > self.args.max_seq_len:
